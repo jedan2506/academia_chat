2d522976e70361f16ab6888e9c22ea5715331dee
parthpathak <parth.pathak@science.christuniversity.in>
2025-11-27 16:19:18 +0530

    cleanup

--- client/src/components/SEOHead.tsx
+++ client/src/components/SEOHead.tsx
@@ -21,21 +21,16 @@ const SEOHead = ({
 
     return (
         <Helmet>
-            {/* Basic Meta Tags */}
             <title>{fullTitle}</title>
             <meta name="description" content={description} />
             <meta name="keywords" content={keywords} />
             <link rel="canonical" href={url} />
-
-            {/* Open Graph */}
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
             <meta property="og:type" content={type} />
             <meta property="og:url" content={url} />
             <meta property="og:image" content={image} />
             <meta property="og:image:alt" content={`${title} - Virallens Marketing Bot`} />
-
-            {/* Twitter Card */}
             <meta name="twitter:title" content={title} />
             <meta name="twitter:description" content={description} />
             <meta name="twitter:image" content={image} />
